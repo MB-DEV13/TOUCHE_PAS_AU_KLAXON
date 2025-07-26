42f6932bed0b2ec1e1ca0d554f26eeb3ae34a51d
MB DEV <mbdev.web13@gmail.com>
2025-07-26 18:02:41 +0200

    recuperation bdd

---
+++ app/Core/Database.php
@@ -0,0 +1,29 @@
+<?php
+// app/Core/Database.php
+
+require_once __DIR__ . '/../../config/config.php';
+
+class Database
+{
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
+        try {
+            $this->pdo = new PDO($dsn, DB_USER, DB_PASS, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            die('Erreur connexion BDD: ' . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance->pdo;
+    }
+}
