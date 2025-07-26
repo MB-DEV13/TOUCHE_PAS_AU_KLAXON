60ccfd0a96e707248383d81a5a8614b530a4bc7d
MB DEV <mbdev.web13@gmail.com>
2025-07-26 17:42:49 +0200

    header simple (logo+btn connexion) + footer - OK

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Touche pas au klaxon</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/TOUCHE_PAS_AU_KLAXON/public/assets/css/style.css">
+</head>
+<body class="d-flex flex-column min-vh-100">
+  <header class="d-flex justify-content-center my-4">
+    <div class="d-flex align-items-center justify-content-between header-box w-100" style="max-width: 900px;">
+        <span class="fs-4 fw-bold app-title">
+          Touche pas au klaxon
+        </span>
+        <a href="/TOUCHE_PAS_AU_KLAXON/public/login" class="btn btn-dark rounded-pill px-4 btn-login">
+          Connexion
+        </a>
+    </div>
+  </header>
+
+
+
+
