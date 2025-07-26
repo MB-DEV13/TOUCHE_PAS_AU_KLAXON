9f2aac21e3488c10976e0801cd3333b44fdfe30e
MB DEV <mbdev.web13@gmail.com>
2025-07-26 22:52:29 +0200

    pop up info user - OK

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -3,6 +3,9 @@
   <span class="fw-bold">© 2024 - TOUCHE_PAS_AU_KLAXON</span>
   <span class="text-muted small ms-2">- MVC PHP</span>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
 
